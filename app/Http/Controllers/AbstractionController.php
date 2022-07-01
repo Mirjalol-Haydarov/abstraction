b2cac6db96977c9ee93cf998873f73dbd2ed7577
Mirjalol-Haydarov <mirjalolhaydarov1@gmail.com>
2022-07-01 06:15:17 +0300

    abstraction

---
+++ app/Http/Controllers/AbstractionController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class AbstractionController extends Controller
+{
+    protected $service;
+protected $storeRequestClass;
+protected $updateRequestClass;
+      
+    public function index()
+    {
+        
+        $items = $this->service->index();
+        return response()->json(['items'=> $items]);
+    } 
+    
+    public function show($id)
+    { 
+         $item = $this->service->show($id);
+        return response()->json(['product'=> $item]);
+    }
+    
+    public function store()
+    {
+          $request = new $this->storeRequestClass;
+          $item = $this->service->store($request->validated()); 
+        return  response()->json(['item' => $item]);
+    } 
+
+    public function update( $id)
+    {
+        $request = new $this->updateRequestClass;
+         $item = $this->service->update($request->validated(), $id);
+        return  response()->json(['item' => $item]);
+    }  
+     
+
+}
